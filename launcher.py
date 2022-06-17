df9b69393ba07377ed4fb273b602c77ea43dc856
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-17 13:40:38 +0200

    line_1 add

--- launcher.py
+++ launcher.py
@@ -1,9 +1,14 @@
 from enum import Flag
 import imp
 from sys import byteorder
+from tkinter import Scale
+from turtle import position
 from ursina import * 
 
 app = Ursina()
+
+
+line_1 = Entity(model = 'cube', color = rgb(79, 70, 70), position = (-5,0), scale = (0.05, 8, 0), )
 
 window.color = rgb(47, 45, 45)
 window.fps_counter.enabled = False
@@ -15,5 +20,6 @@ window.borderless = False
 window.cog_menu = False
 window.cog_button = False
 window.icon = 'game_icon2.ico'
+
 
 app.run()
